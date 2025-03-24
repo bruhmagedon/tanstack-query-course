8a27cdd2baa86745497bef8c135f1a4305804d70
bruhmagedon <schubert.walk@yandex.ru>
2025-03-24 19:47:12 +0300

    init

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "../shared/api/queryClient";
+import { TodoList } from "../modules/todo-list/todo-list";
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TodoList />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
